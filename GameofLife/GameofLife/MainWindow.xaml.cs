536fe9763f28d9b4603ee8bccce27258c98924a5
Patrick <Patrickbjerre@outlook.dk>
2022-05-17 21:04:59 +0200

    added the main window with a stack panel

---
+++ GameofLife/GameofLife/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameofLife
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {       
+            InitializeComponent();
+            Image im = new Image();
+            
+            
+            for (int i = 0; i < 10000; i++)
+            {
+                for(int j = 0; j < 10000; j++)
+                {
+                    if(i == j)
+                    {
+                       
+                     
+                    }
+                   
+                }
+            }
+           
+        }
+    }
+}
