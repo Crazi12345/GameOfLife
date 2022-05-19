002b2fdc8ef6130da871cfa875ebedccca0dca46
Patrick <Patrickbjerre@outlook.dk>
2022-05-19 12:14:31 +0200

    now with a better ui and a grid manipulation

---
+++ GameofLife/GameofLife/GridHandler.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+namespace GameofLife
+{
+    internal class GridHandler
+    {
+        private Button[,] buttons;
+        private int x;
+        private int y;
+        public GridHandler(int x, int y)
+        {
+            this.buttons = generateButtons(x, y);
+            this.x = x;
+            this.y = y;
+        }
+
+        private Button[,] generateButtons(int x, int y)
+        {
+            Button[,] button_list = new Button[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++) {
+                  Button b = new Button();
+                    b.Height = 50;
+                    b.Width = 50;
+                   
+                    b.Background = new SolidColorBrush(Colors.Black);
+                    b.Click += new EventHandler(ChangeColor).Invoke;
+                    button_list[i, j] = b;
+                }
+
+            }
+            return button_list;
+        }
+
+        public Button getButton(int x, int y)
+        {
+            return buttons[x, y];
+        }
+
+        public int getX()
+        {
+            return x;
+
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+        private void ChangeColor(object sender, EventArgs e)
+        {
+            var but = sender as Button;
+
+            if (((SolidColorBrush)but.Background).Color == Colors.Black)
+            {
+                but.Background = new SolidColorBrush(Colors.White);
+            }
+            else
+            {
+                but.Background = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        public void moveDown()
+        {
+            for (int i = x - 1; i > 0; i--)
+            {
+
+                for (int j = 0; j < y; j++)
+                {
+                    Button b = buttons[i, j];
+                    if (((SolidColorBrush)b.Background).Color == Colors.White)
+                    {
+                        buttons[i, j].Background = new SolidColorBrush(Colors.Black);
+                        buttons[i + 1, j].Background = new SolidColorBrush(Colors.White);
+                    }
+                }
+            }
+        }
+            public void moveUp()
+            {
+                for (int i =0; i <x-1; i++)
+                {
+
+                    for (int j = 0; j < y; j++)
+                    {
+                        Button b = buttons[i, j];
+                        if (((SolidColorBrush)b.Background).Color == Colors.White)
+                        {
+                            buttons[i, j].Background = new SolidColorBrush(Colors.Black);
+                            buttons[i -1, j].Background = new SolidColorBrush(Colors.White);
+                        }
+                    }
+                }
+            }
+
+        public void moveLeft()
+        {
+            for (int i = 0; i < x - 1; i++)
+            {
+
+                for (int j = y-1; j > 0; j--)
+                {
+                    Button b = buttons[i, j];
+                    if (((SolidColorBrush)b.Background).Color == Colors.White)
+                    {
+                        buttons[i, j].Background = new SolidColorBrush(Colors.Black);
+                        buttons[i, j-1].Background = new SolidColorBrush(Colors.White);
+                    }
+                }
+            }
+        }
+    }
+    
+}
--- GameofLife/GameofLife/MainWindow.xaml.cs
+++ GameofLife/GameofLife/MainWindow.xaml.cs
@@ -20,25 +20,111 @@ namespace GameofLife
     /// </summary>
     public partial class MainWindow : Window
     {
+        GridHandler gridHandler = new GridHandler(100, 100);
+        public bool isOpening = true;
+        public StackPanel[] stackPanels = new StackPanel[50];
         public MainWindow()
-        {       
+        {
             InitializeComponent();
-            Image im = new Image();
-            
-            
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < stackPanels.Length; i++)
             {
-                for(int j = 0; j < 10000; j++)
-                {
-                    if(i == j)
-                    {
-                       
-                     
-                    }
-                   
-                }
+                stackPanels[i] = new StackPanel();
             }
-           
+
+            printGrid(0, 0,50);
+
+
+        }
+
+        public void printGrid(int xOffset, int yOffset, int scale)
+        {
+          
+            int x = gridHandler.getX() ;
+            int y = gridHandler.getY() ;
+            GridPanel.Children.Clear();
+            for (int i = x / 2; i < x; i++)
+            {
+                
+                stackPanels[x%stackPanels.Length] = new StackPanel();
+                stackPanels[x % stackPanels.Length].Orientation = Orientation.Horizontal;
+               
+                GridPanel.Children.Add(stackPanels[x % stackPanels.Length]);
+             
+            
+                for (int j = y / 2; j < y; j++)
+                {
+                    Button b = gridHandler.getButton(i, j);
+                    b.Height = scale;
+                    b.Width = scale;
+                    stackPanels[x % stackPanels.Length].Children.Remove(b);
+                    if (isOpening == true)
+                    {
+                        stackPanels[x % stackPanels.Length].Children.Remove((Button)b);
+
+                        stackPanels[x % stackPanels.Length].Children.Add(b);
+                       
+                      
+                    }
+                  
+
+
+                }
+               
+            }
+        }
+
+       
+      
+        
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            
+           // printGrid(0, 0,(int)ScaleSlider.Value);
+        }
+
+        private void StartBut_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+        }
+        public void reload()
+        {
+          
+            for (int i = 0; i < stackPanels.Length; i++)
+            {
+                if (stackPanels[i].Children.Count > 0)
+                {
+                    stackPanels[i].Children.RemoveAt(stackPanels[i].Children.Count - 1);
+                }
+                stackPanels[i].Children.Clear();
+                stackPanels[i] = new StackPanel();
+            }
+            GridPanel.Children.Clear();
+
+        }
+        private void addBut_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void DownBut_Click(object sender, RoutedEventArgs e)
+        {
+            gridHandler.moveDown();
+        }
+
+    
+
+        private void UpBut_Click_1(object sender, RoutedEventArgs e)
+        {
+            gridHandler.moveUp();
         }
     }
-}
+
+      
+
+
+
+
+
+    }
+
+
